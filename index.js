0315b4784389443f755920a689df1bc7fe7afff5
Rain_Jiang <rain.jiang@walmart.com>
2025-01-21 16:10:24 +0800

    fix getter error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-draggable-flatlist';
 
 import {ScriptManager, Script, Federated} from '@callstack/repack/client';
 
